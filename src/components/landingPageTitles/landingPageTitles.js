d18aa5648c6e0d2bd726ac4b0ecfbcf8d802c85d
Charles Floyd <charleswfloyd3@gmail.com>
2021-05-23 22:26:46 -0600

    changes

--- src/components/landingPageTitles/landingPageTitles.js
+++ src/components/landingPageTitles/landingPageTitles.js
@@ -1,13 +1,16 @@
 import React from 'react'
-
+import AOS from 'aos';
+import 'aos/dist/aos.css'; 
 
 import "./landingPageTitles.css"
 const LandingPageTitles = (props) => {
+AOS.init();
+
     return (
         <div className="landingPageTitles">
             <div className="landingPageTitles-inner">
-                <p className="productTitle">{props.product}</p>
-                <p className="productSubHeading">{props.subHeading}</p>
+                <p className="productTitle" data-aos="fade-up" data-aos-duration="800">{props.product}</p>
+                <p className="productSubHeading" data-aos="fade-in" data-aos-duration="1200">{props.subHeading}</p>
             </div>
         </div>
     )
